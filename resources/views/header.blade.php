601deaa958dd3614a0638ca8194fdfde75d35739
shohely1710 <66716089+shohely1710@users.noreply.github.com>
2021-03-23 19:56:24 +0600

    Cart Product Listing done

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -34,7 +34,7 @@ if(Session::has('user'))
         <button type="submit" class="btn btn-default">Search</button>
       </form>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Cart({{$total}})</a></li>
+        <li><a href="/cartlist">Cart({{$total}})</a></li>
           @if(Session::has('user'))
 
           <li class="dropdown">
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -57,5 +57,11 @@
         width: 500px !important;
 
     }
+    .cart-list-divider
+    {
+        border-bottom: 1px solid #ccc;
+        margin-bottom: 20px;
+        padding-bottom: 20px;
+    }
 </style>
 </html>
